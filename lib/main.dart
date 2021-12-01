01e069063eb2c493375c54374d9d93035af8f0fd
Aayush Regmi <aayush.181509@ncit.edu.np>
2021-12-01 19:01:21 +0545

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,61 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Text("This is a Column Widget"),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+                margin: EdgeInsets.all(30),
+                padding: EdgeInsets.all(30.0),
+                child: Text("Green Color"),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+                margin: EdgeInsets.all(30),
+                padding: EdgeInsets.all(30.0),
+                child: Text("Green Color"),
+              ),
+              Image(
+                height: 200,
+                image: NetworkImage(
+                    'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: Text('Row'),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                    margin: EdgeInsets.all(30),
+                    padding: EdgeInsets.all(30.0),
+                    child: Text("Green Color"),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                    margin: EdgeInsets.all(30),
+                    padding: EdgeInsets.all(30.0),
+                    child: Text("Green Color"),
+                  ),
+                  Image(
+                    height: 200,
+                    image: NetworkImage(
+                        'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
